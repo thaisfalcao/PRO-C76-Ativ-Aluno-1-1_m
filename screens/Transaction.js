eed8c0bd6690dcb85bbcb8903a4b576d6c8f372e
thaisfalcao <101199332+thaisfalcao@users.noreply.github.com>
2022-12-15 14:00:02 -0300

    Add files via upload

---
+++ screens/Transaction.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default class TransactionScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: ""
+    };
+  }
+
+  getCameraPermissions = async domState => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      /*status === "granted" é verdadeiro se o usuário concedeu permissão
+          status === "granted" é falso se o usuário não concedeu permissão
+        */
+      hasCameraPermissions: status === "granted",
+      domState: domState,
+      scanned: false
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <TouchableOpacity
+          style={[styles.button, { marginTop: 25 }]}
+          onPress={() => this.getCameraPermissions("scanner")}
+        >
+          <Text style={styles.buttonText}>Digitalizar QR Code</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5653D4"
+  },
+  text: {
+    color: "#ffff",
+    fontSize: 15
+  }
+ 
+});
